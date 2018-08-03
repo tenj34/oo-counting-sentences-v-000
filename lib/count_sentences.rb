a1381058e0fe21665121305b9ff46960b2bd6183
Juan Tenezaca <tenj34@gmail.com>
2018-08-03 17:27:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/[\.?!]/).count {|sentence| sentence != ""}
+    # we split the the sentences up with specific symbols that we sentences
+    # count for every sentence that is not empty
   end
 end
