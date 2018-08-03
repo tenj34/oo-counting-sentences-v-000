3ba191989c430d4d67bbf5f5689096d7b648424f
Juan Tenezaca <tenj34@gmail.com>
2018-08-03 17:29:04 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,6 @@ class String
   def count_sentences
     self.split(/[\.?!]/).count {|sentence| sentence != ""}
     # we split the the sentences up with specific symbols that we sentences
-    # count for every sentence that is in that splits
+    # count for every sentence that is in that split
   end
 end
